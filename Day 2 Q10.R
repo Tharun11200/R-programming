fa26032eb7aeb833cbcbfba75d4f3578f55cd3c9
THARUN <85387935+Tharun11200@users.noreply.github.com>
2023-02-06 14:09:15 +0530

    Delete Day 2 Q10.R

--- Day 2 Q10.R
+++
@@ -1,9 +0,0 @@
-data("airquality")
-if (is.data.frame(airquality)){
-  print("airquality is a data frame.")
-}else {
-  print("airquality is not a data frame.")
-}
-airquality <- airquality[order(airquality$Ozone, airquality$Month),]
-airquality <- airquality[ , !(names(airquality) %in% c("Solar.R", "Wind"))]
-print(airquality)
